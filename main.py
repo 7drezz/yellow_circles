1c52b3ff0835499f400d7086a507f9cf7e94c6b1
7drezz <fedor06032007@gmail.com>
2023-11-18 18:59:06 +0300

    Random color circle

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import sys
 import random
 
-from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 from PyQt5.QtGui import QPainter, QColor
 
 
 class Window(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
-        self.button.clicked.connect(self.function)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(300, 300, 600, 500)
+        self.button = QPushButton('Тык', self)
+        self.button.resize(141, 41)
+        self.button.move(240, 360)
         self.flag = None
+
+        self.button.clicked.connect(self.function)
 
     def function(self):
         self.flag = True
@@ -29,7 +35,7 @@ class Window(QWidget):
         x = random.randint(100, 400)
         y = random.randint(100, 200)
         r = random.randint(1, 90)
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
         qp.drawEllipse(x, y, r, r)
 
 
